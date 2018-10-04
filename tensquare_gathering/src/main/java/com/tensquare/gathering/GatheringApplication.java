eae3b1e604fc2878e6b578fdb845a324135ae63a
ZhangDeZhe <1392777056@qq.com>
2018-10-04 17:46:12 +0800

    活动微服务，并且加入redis注解和redisTemplate之间的区别. 就在于有没有有效的时间设置。

---
+++ tensquare_gathering/src/main/java/com/tensquare/gathering/GatheringApplication.java
@@ -0,0 +1,20 @@
+package com.tensquare.gathering;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import utils.IdWorker;
+@SpringBootApplication
+@EnableCaching // 开启缓存支持
+public class GatheringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatheringApplication.class, args);
+	}
+
+	@Bean
+	public IdWorker idWorkker(){
+		return new IdWorker(1, 1);
+	}
+	
+}
