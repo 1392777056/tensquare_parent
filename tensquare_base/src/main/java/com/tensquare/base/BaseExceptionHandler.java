2812fd7ddfda1d597b67eb01af6c779b35629ca2
ZhangDeZhe <1392777056@qq.com>
2018-10-04 12:24:32 +0800

    定义一个全局的异常通知

--- tensquare_base/src/main/java/com/tensquare/base/BaseExceptionHandler.java
+++ tensquare_base/src/main/java/com/tensquare/base/BaseExceptionHandler.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * 统一异常处理类
  */
 @ControllerAdvice
-
 public class BaseExceptionHandler {
 
     @ExceptionHandler(value = Exception.class)
