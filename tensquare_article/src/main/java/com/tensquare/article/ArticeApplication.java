e8d5bc613b59cd09d3d6fd445e28f9120167d59a
ZhangDeZhe <1392777056@qq.com>
2018-10-24 22:02:08 +0800

    article加入eureka的客户端完成

--- tensquare_article/src/main/java/com/tensquare/article/ArticeApplication.java
+++ tensquare_article/src/main/java/com/tensquare/article/ArticeApplication.java
@@ -1,9 +1,11 @@
 package com.tensquare.article;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import utils.IdWorker;
 @SpringBootApplication
+@EnableEurekaClient
 public class ArticeApplication {
 
 	public static void main(String[] args) {
